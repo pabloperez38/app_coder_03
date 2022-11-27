198de2dc14f2b3a3a07c98ce36fc2269a1ca11b4
cirilo38 <pablo.eluniversoweb@gmail.com>
2022-11-27 14:45:37 -0300

    Agregando paleta de colores

--- src/navegacion/shop.js
+++ src/navegacion/shop.js
@@ -1,14 +1,25 @@
 import { Categorias, Producto, Productos } from "../pantallas";
 
 import React from "react";
+import { colores } from "../constantes/tema/colores";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 const Stack = createNativeStackNavigator();
 
 const ShopNavigator = () => {
   return (
-    <Stack.Navigator initialRouteName="Categorias">
-      <Stack.Screen name="Categorias" component={Categorias} />
+    <Stack.Navigator
+      initialRouteName="Categorias"
+      screenOptions={{
+        headerStyle: { backgroundColor: colores.secundario },
+        headerTintColor: colores.blanco,
+      }}
+    >
+      <Stack.Screen
+        name="Categorias"
+        component={Categorias}
+        options={{ title: "Categorías" }}
+      />
       <Stack.Screen name="Productos" component={Productos} />
       <Stack.Screen name="Producto" component={Producto} />
     </Stack.Navigator>
--- src/pantallas/categorias/index.js
+++ src/pantallas/categorias/index.js
@@ -1,12 +1,18 @@
-import { Text, View } from "react-native";
+import { Button, Text, View } from "react-native";
 
 import React from "react";
+import { colores } from "../../constantes/tema/colores";
 import { styles } from "./styles";
 
-const Categorias = () => {
+const Categorias = ({ navigation }) => {
   return (
     <View style={styles.container}>
       <Text style={styles.titulo}>Categorías</Text>
+      <Button
+        title="Ir a productos"
+        color={colores.secundario}
+        onPress={() => navigation.navigate("Productos")}
+      />
     </View>
   );
 };
