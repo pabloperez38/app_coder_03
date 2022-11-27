2ce60b1e7105bdc73ad0507cb980bf2040be587a
cirilo38 <pablo.eluniversoweb@gmail.com>
2022-11-27 11:51:13 -0300

    Creación de pantallas

---
+++ src/pantallas/productos/index.js
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+
+import React from "react";
+import { styles } from "./styles";
+
+const Productos = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Productos</Text>
+    </View>
+  );
+};
+
+export default Productos;
